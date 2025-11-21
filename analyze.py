2dd8af19718cb4621163cbc2f9e3dfd6277cd306
Miracle Kalu <miminwakalu@gmail.com>
2025-11-22 00:33:05 +0100

    Added user input validation

--- analyze.py
+++ analyze.py
@@ -6,12 +6,17 @@ def welcomeUser():
 def getUserName():
     # Print message prompting user to input their name
     usernameFromInput = input("\nTo get started, please enter your name:\n")
+    
+    if len(usernameFromInput) < 5 or not usernameFromInput.isIdentifier():
+        print("Your username must be at least 5 characters long, alphanumeric only(a-z/A-Z/0-9), have no spaces, and cannot start with a number! ")
+        print("Assigning username instead...")
+        return generate_username() [0]
+
     return usernameFromInput
 
 # Greet the user
 def greetUser(name):
     print("Hello, " + name)
-
 
 welcomeUser()
 username = getUserName()
