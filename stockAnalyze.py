922c5296976b1f083716074cb685e9f45328f6d0
Miracle Kalu <miminwakalu@gmail.com>
2026-01-29 15:57:39 +0100

    Got basic stock info

--- stockAnalyze.py
+++ stockAnalyze.py
@@ -1,5 +1,7 @@
+from datetime import date
+from typing import Any
+from datetime import datetime
 import yfinance as yf
-
 def extractBasicStockInfo(data):
     keysToExtract = [ 'longName', 'website', 'industry', 'marketCap', 'previousClose', 'totalRevenue', 'bookValue' ]
     basicInfo = {}
@@ -7,12 +9,47 @@ def extractBasicStockInfo(data):
         if key in data:
             basicInfo[key] = data[key]
         else:
-            basicInfo[key] = ""
+            basicInfo[key] = ''
     return basicInfo
 
-def getCompanyStockInfo(tickerSymbol):
-    company = yf.Ticker(tickerSymbol)
-    basicInfo = extractBasicStockInfo(company.info)
-    print(basicInfo)
+def getPriceHistory(company):
+    historyDf = company.history(period="12mo")
+    prices = historyDf['Open'].tolist()
+    dates = historyDf.index.strftime('%Y-%m-%d').tolist()
+    print(dates)
+    return {
+        'prices': prices,
+        'dates': dates
+    }
 
-getCompanyStockInfo("MSFT")
+def getEarningsDate(company):
+    earningsDatesDf = company.earnings_dates
+    allDates = earningsDatesDf.index.strftime('%Y-%m-%d').tolist()
+    dateObjects = [date.strptime(date_str, '%Y-%m-%d').date() for date_str in allDates]
+    currentDate = date.today()
+    futureDates = [date.strftime('%Y-%m-%d') for date in dateObjects if date > currentDate]
+    return futureDates
+
+def getCompanyNews(company):
+    news = company.news
+    allNewsArticles = []
+    for newDict in news:
+        newsDictToAddd = {
+            'title': newDict['title'],
+            'link': newDict['link']
+        }
+        allNewsArticles.append(newsDictToAddd)
+    return allNewsArticles
+
+def getCompanyStockInfo(tickerSymbol):
+    # Get data from Yahool Finance API
+    company = yf.Ticker(tickerSymbol)
+
+    # Get basic info on company
+    basicInfo = extractBasicStockInfo(company.info)
+    priceHistory = getPriceHistory(company)
+    futureEarningsDates = getEarningsDate(company)
+    newsArticles = getCompanyNews(company)
+   # print(priceHistory)
+
+getCompanyStockInfo('MSFT')
